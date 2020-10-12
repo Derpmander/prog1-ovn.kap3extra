1df8a4ad182cf597c3ce15c04f104b360136f7e4
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-10-12 15:04:01 +0200

    r

--- 3.2.py
+++ 3.2.py
@@ -2,11 +2,11 @@ import math#importera math
 rad=input("Vad är cirkelns radie: ") #tar reda på cirkelns radie
 x=float(rad)#gör om str till float
 if x>0: #gör så att om radien är större än 0 så fortsäter den räkna
-    y=3.141
-    area=x*x*y
-    print(f"arean är {area:2.3f}")
-    z=x+x
-    omkrets=z*y
-    print(f"omkretsen är {omkrets:2.3f}") 
+    y=3.141#radien
+    area=x*x*y#räknar ut arean
+    print(f"arean är {area:2.3f}")#skriver ut arean
+    z=x+x#räknar ut diamentern
+    omkrets=z*y#räknar ut omkrets
+    print(f"omkretsen är {omkrets:2.3f}")#skriver ut omkretsen
 else:
-    print("error")
+    print("error")#skriver ut error om det var mindre än noll
---
+++ 3.3.py
@@ -0,0 +1,4 @@
+import math
+a=float(input("Vad är nummer 1: "))
+
+c=math.sqrt(a**2+b**2-2ab*cos(a))
