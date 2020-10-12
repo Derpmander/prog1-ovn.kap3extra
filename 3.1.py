3ebcb568367d0f0daae35ed406d6cf4a89ab2232
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-10-12 14:29:13 +0200

    lite text fix

--- 3.1.py
+++ 3.1.py
@@ -1,7 +1,7 @@
 minn=int(input("Hur lång tid använder du för att ringa per månad?: "))
 if minn>0 and minn<=33:
-    print("kontant")
+    print("Välj kontant")
 elif minn>33 and minn <=66:
-    print("normal")
+    print("Välj normal")
 elif minn>66 and minn <=100000:
-    print("plus")
+    print("Välj plus")
