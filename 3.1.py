b309e2c4c2a8e3485c40ffd3d98005a41d6fb01f
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-10-12 14:28:32 +0200

    Fungerar

--- 3.1.py
+++ 3.1.py
@@ -1,9 +1,7 @@
 minn=int(input("Hur lång tid använder du för att ringa per månad?: "))
-if minn>0<=33:
+if minn>0 and minn<=33:
     print("kontant")
-elif minn<33>=66:
+elif minn>33 and minn <=66:
     print("normal")
-elif minn<66>=100000:
+elif minn>66 and minn <=100000:
     print("plus")
-else:
-    print ("YOU FUCKED UP")
